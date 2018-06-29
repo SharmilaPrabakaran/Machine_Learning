6e3ab021b359a8b9ef14121ad5d5aed822372651
Sharmila <ppsharmi@gmail.com>
2018-06-29 14:50:40 -0700

    deleted wrong repository

--- array-last.js
+++
@@ -1,9 +0,0 @@
-// find last element of the array
-
-a = ['abc','family','badminton','san jose']
-function last(a){
-  var n = a.length;
-  var b = a[n-1];
-  return b;
-}
-console.log(last(a))//san jose
